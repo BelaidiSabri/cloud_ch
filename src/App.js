0ae5f41b821ff20106668115b87649e86b69e7a8
Belaidi Sabri <belaidisabri@gmail.com>
2023-05-24 17:25:52 +0100

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MapContainer from './components/MapContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <MapContainer/>
+    </div>
+  );
+}
+
+export default App;
